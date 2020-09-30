ac6a6b2308650653bea708bebfc1676126282984
JezzyDeves <juncrhepo@gmail.com>
2020-09-30 15:34:49 -0400

    Added 02_Claims challenge files

---
+++ ClaimsRepository/Claim.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimsRepository
+{
+    public enum ClaimType { Car, Home, Theft}
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+        public ClaimType TypeOfClaim { get; set; }
+        public string Description { get; set; }
+        public double ClaimAmmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+
+        public Claim()
+        {
+
+        }
+        public Claim(int id, ClaimType claimType, string description, double ammount, DateTime dateOfIncident, DateTime dateOfClaim)
+        {
+            ClaimID = id;
+            TypeOfClaim = claimType;
+            Description = description;
+            ClaimAmmount = ammount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+        }
+    }
+}
---
+++ ClaimsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClaimsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClaimsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0490f932-faa9-445c-8b9e-80a8166e0e03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
