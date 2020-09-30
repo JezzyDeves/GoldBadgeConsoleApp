3ff02f90162b5ef9bdcc66cd87ac66c06d5b4eef
JezzyDeves <juncrhepo@gmail.com>
2020-09-30 16:45:14 -0400

    Added GetAllClaims test

--- ClaimsTests/ClaimsTests.cs
+++ ClaimsTests/ClaimsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ClaimsRepository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,6 +8,19 @@ namespace ClaimsTests
     [TestClass]
     public class ClaimsTests
     {
+        private Claim _item;
+        private ClaimRepo _repo;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            DateTime claimDay = new DateTime(2000, 11, 11);
+            DateTime incidentDay = new DateTime(2000, 11, 12);
+            _item = new Claim(1, ClaimType.Car, "Car broken", 400, incidentDay, claimDay);
+            _repo = new ClaimRepo();
+            _repo.AddClaim(_item);
+        }
+
         [TestMethod]
         public void AddClaim_ShouldReturnTrue()
         {
@@ -16,6 +30,13 @@ namespace ClaimsTests
             ClaimRepo repo = new ClaimRepo();
             bool addClaim = repo.AddClaim(claim);
             Assert.IsTrue(addClaim);
+        }
+        [TestMethod]
+        public void GetAllClaims_ShouldReturnTrue()
+        {
+            List<Claim> claims = _repo.GetAllClaims();
+            bool list = claims.Contains(_item);
+            Assert.IsTrue(list);
         }
     }
 }
