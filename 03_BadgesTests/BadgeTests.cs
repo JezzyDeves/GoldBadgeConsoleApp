1f5030b740080f07c61a49845467617ec69150ef
JezzyDeves <juncrhepo@gmail.com>
2020-10-02 11:50:28 -0400

    AddBadgeDoors test returned true

--- 03_BadgesTests/BadgeTests.cs
+++ 03_BadgesTests/BadgeTests.cs
@@ -8,15 +8,31 @@ namespace _03_BadgesTests
     [TestClass]
     public class BadgeTests
     {
+        private Badge _item;
+        private BadgeRepo _repo;
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new BadgeRepo();
+            List<string> doors = new List<string>() { "A5", "B12" };
+            _item = new Badge(1, doors);
+            _repo.AddBadge(_item);
+        }
         [TestMethod]
         public void AddBadge_ShouldReturnTrue()
         {
             BadgeRepo repo = new BadgeRepo();
             List<string> doors = new List<string>() { "A5", "B12" };
-            Badge badge = new Badge(doors, 1);
+            Badge badge = new Badge(1, doors);
             bool addBadge = repo.AddBadge(badge);
 
             Assert.IsTrue(addBadge);
+        }
+        [TestMethod]
+        public void UpdateBadge()
+        {
+            bool update = _repo.AddBadgeDoors(1, "B2");
+            Assert.IsTrue(update);
         }
     }
 }
