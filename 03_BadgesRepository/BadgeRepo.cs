6d7a70747a4fa0507e8a87a6cc1435055150e942
JezzyDeves <juncrhepo@gmail.com>
2020-10-02 10:01:37 -0400

    Added AddBadge method and tests

---
+++ 03_BadgesRepository/BadgeRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_BadgesRepository
+{
+    public class BadgeRepo
+    {
+        private readonly Dictionary<int, List<string>> _badges = new Dictionary<int, List<string>>();
+
+        public bool AddBadge(Badge badge)
+        {
+            int id = badge.BadgeID;
+            List<string> doors = badge.BadgeDoors;
+
+            _badges.Add(id, doors);
+            bool added = _badges.ContainsKey(id);
+            return added;
+        }
+    }
+}
--- 03_BadgesTests/BadgeTests.cs
+++ 03_BadgesTests/BadgeTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using _03_BadgesRepository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace _03_BadgesTests
@@ -7,8 +9,14 @@ namespace _03_BadgesTests
     public class BadgeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddBadge_ShouldReturnTrue()
         {
+            BadgeRepo repo = new BadgeRepo();
+            List<string> doors = new List<string>() { "A5", "B12" };
+            Badge badge = new Badge(doors, 1);
+            bool addBadge = repo.AddBadge(badge);
+
+            Assert.IsTrue(addBadge);
         }
     }
 }
