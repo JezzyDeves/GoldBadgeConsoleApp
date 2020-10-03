14fdd41bdb5777e660eaf873d3c5a11e914b0482
JezzyDeves <juncrhepo@gmail.com>
2020-10-03 12:08:44 -0400

    Tested RemoveBadgeDoors and GetAllBadges

--- 03_BadgesTests/BadgeTests.cs
+++ 03_BadgesTests/BadgeTests.cs
@@ -29,10 +29,34 @@ namespace _03_BadgesTests
             Assert.IsTrue(addBadge);
         }
         [TestMethod]
-        public void UpdateBadge()
+        public void AddBadgeDoor_ShouldReturnTrue()
         {
             bool update = _repo.AddBadgeDoors(1, "B2");
             Assert.IsTrue(update);
+        }
+        [TestMethod]
+        public void RemoveBadgeDoor_ShouldReturnTrue()
+        {
+            bool update = _repo.RemoveBadgeDoors(1, "A5");
+            Assert.IsTrue(update);
+        }
+        [TestMethod]
+        public void GetAllBadges_ShouldReturnTrue()
+        {
+            //First test using the initialized arrangement
+            Dictionary<int, List<string>> badges = _repo.GetAllBadges();
+            bool containsKey = badges.ContainsKey(1);
+            Assert.IsTrue(containsKey);
+
+            //Second test looking for both the key and value pair
+            BadgeRepo repo = new BadgeRepo();
+            List<string> doors = new List<string>() { "A5", "B12" };
+            Badge badge = new Badge(1, doors);
+            bool addBadge = repo.AddBadge(badge);
+
+            Dictionary<int, List<string>> valuePairs = repo.GetAllBadges();
+            bool contains = valuePairs.TryGetValue(1, out doors);
+            Assert.IsTrue(contains);
         }
     }
 }
