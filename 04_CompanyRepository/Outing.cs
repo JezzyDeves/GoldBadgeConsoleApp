bc8c8c708d237e0d06d9a70335a34dc6725e4a9a
JezzyDeves <juncrhepo@gmail.com>
2020-10-05 16:21:58 -0400

    Added outing object

--- 04_CompanyRepository/Outing.cs
+++ 04_CompanyRepository/Outing.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace _04_CompanyRepository
 {
+    public enum EventType { Golf, Bowling, AmusementPark, Concert };
     public class Outing
     {
+        public EventType TypeOfEvent { get; set; }
+        public int People { get; set; }
+        public DateTime Date { get; set; }
+        public double CostPerson { get; set; }
+        public double CostTotal { get; set; }
+
+        public Outing()
+        {
+
+        }
+        public Outing(EventType type, int people, DateTime date, double personCost, double totalCost)
+        {
+            TypeOfEvent = type;
+            People = people;
+            Date = date;
+            CostPerson = personCost;
+            CostTotal = totalCost;
+        }
     }
 }
