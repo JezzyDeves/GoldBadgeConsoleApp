80c768820cd2086548286936e8c29904107011e8
JezzyDeves <juncrhepo@gmail.com>
2020-10-06 10:17:12 -0400

    Added GetOutings method

--- 04_CompanyTests/CompanyRepoTests.cs
+++ 04_CompanyTests/CompanyRepoTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using _04_CompanyRepository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,6 +8,16 @@ namespace _04_CompanyTests
     [TestClass]
     public class CompanyRepoTests
     {
+        private CompanyRepo _repo;
+        private Outing _outing;
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new CompanyRepo();
+            DateTime dateTime = new DateTime(2000, 12, 12);
+            _outing = new Outing(EventType.Golf, 100, dateTime, 15.45, 2000);
+            _repo.AddOuting(_outing);
+        }
         [TestMethod]
         public void AddOuting_ShouldReturnTrue()
         {
@@ -15,6 +26,13 @@ namespace _04_CompanyTests
             Outing outing = new Outing(EventType.Golf, 100, dateTime, 15.45, 2000);
             bool added = _repo.AddOuting(outing);
             Assert.IsTrue(added);
+        }
+        [TestMethod]
+        public void GetOutings_ShouldReturnCorrectValue()
+        {
+            List<Outing> outings = _repo.GetOutings();
+            bool containsItem = outings.Contains(_outing);
+            Assert.IsTrue(containsItem);
         }
     }
 }
